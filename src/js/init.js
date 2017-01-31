5db25ab0436308cb8ec2e3e3ffb03918fa601e0c
azarz <amaury.zarzelli@ensg.eu>
2017-01-31 14:21:51 +0100

    position de l'iss trouvée

--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,16 @@
+//************* Variables globales ********************
+
+var timeout = 2000; //période de rafraichissement, en ms
+var zoom = 4;
+
+
+
 function init() {
     // On initialise la carte
-    var mymap = L.map('mapid').setView([0.0, 0.0], 13);
+    var mymap = L.map('mapid').setView([0.0, 0.0], zoom);
+    var marker;
+
+    var latlontxt = document.getElementById("latlontxt");
 
     var lat = 0;
     var lon = 0;
@@ -11,6 +21,29 @@ function init() {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
 
+    var ajax = new XMLHttpRequest();
+
+    ajax.addEventListener('readystatechange',  function(e) {
+        // si l'état est le numéro 4 et que la ressource est trouvée
+        if(ajax.readyState == 4 && ajax.status == 200) {
+            //S'il y a déjà un marker, on le retire
+            if(marker){
+                mymap.removeLayer(marker);
+            }
+            //On met à jour la position de l'ISS
+            positionISS = JSON.parse(ajax.responseText);
+            var latISS = positionISS.iss_position.latitude;
+            var lonISS = positionISS.iss_position.longitude;
+            //On ajoute notre marker
+            marker = new L.marker([latISS, lonISS]);
+            marker.addTo(mymap);
+            //On déplace notre caméra
+            mymap.setView([latISS, lonISS], zoom);
+            //On met à jour le texte
+            latlontxt.innerHTML = latISS + ", " + lonISS;
+        }
+    });
+
 /*
     var champRech = document.getElementById('form');
     var champ = document.getElementById('champRech');
@@ -19,33 +52,19 @@ function init() {
     champRech.addEventListener('submit', function(e){
     e.preventDefault();
 */
-    var ajax = new XMLHttpRequest();
 
-    ajax.addEventListener('readystatechange',  function(e) {
-        // si l'état est le numéro 4 et que la ressource est trouvée
-        if(ajax.readyState == 4 && ajax.status == 200) {
-            // le texte de la réponse
-            positionISS = JSON.parse(ajax.responseText);
-            var latISS = positionISS.iss_position.latitude;
-            var lonISS = positionISS.iss_position.longitude;
-            var marker = L.marker([latISS, lonISS]).addTo(mymap);
-            mymap.setView([latISS, lonISS], 13);
-        }
-    });
-
-    // données GET éventuelles de la requête AJAX
-    ajax.open("GET","http://api.open-notify.org/iss-now.json",true);
-
-    // envoi de la requête
-    ajax.send();
-
+    timeoutUpdateDate(timeout, ajax);
 }
+
 
 /**************** Gestion du rafraichissement *****************************/
 
-function timeoutUpdateDate(timeOut) {
+function timeoutUpdateDate(timeOut, req) {
     setTimeout(function () {
-        //...
-        timeoutUpdateDate(timeOut);
+        // données GET éventuelles de la requête AJAX
+        req.open("GET","http://api.open-notify.org/iss-now.json",true);
+        // envoi de la requête
+        req.send();
+        timeoutUpdateDate(timeOut, req);
     }, timeOut);
 }

dd3810881fab75ba6c5b19890902700231e9313d
azarz <amaury.zarzelli@ensg.eu>
2017-01-31 18:32:23 +0100

    polyligne, icone, début du contrôle

--- src/js/init.js
+++ src/js/init.js
@@ -1,16 +1,29 @@
 //************* Variables globales ********************
 
-var timeout = 2000; //période de rafraichissement, en ms
-var zoom = 4;
+var timeout = 1000; //période de rafraichissement, en ms
+var zoom = 4;       //niveau de zoom
+
+
+
+//icone de marker personalisée
+var issIcon = L.icon({
+    iconUrl: 'img/logo_ISS.png',
+    iconSize: [38, 38],
+    iconAnchor: [19, 19]
+});
 
 
 
 function init() {
     // On initialise la carte
-    var mymap = L.map('mapid').setView([0.0, 0.0], zoom);
+    var mymap = new L.map('mapid').setView([0.0, 0.0], zoom);
     var marker;
+    var polyline;
+    var lastLong;
 
+    //récupération des éléments HTML
     var latlontxt = document.getElementById("latlontxt");
+    var suiviBtn = document.getElementById("cboxSuivi");
 
     var lat = 0;
     var lon = 0;
@@ -26,21 +39,44 @@ function init() {
     ajax.addEventListener('readystatechange',  function(e) {
         // si l'état est le numéro 4 et que la ressource est trouvée
         if(ajax.readyState == 4 && ajax.status == 200) {
+
             //S'il y a déjà un marker, on le retire
             if(marker){
                 mymap.removeLayer(marker);
             }
+
             //On met à jour la position de l'ISS
             positionISS = JSON.parse(ajax.responseText);
             var latISS = positionISS.iss_position.latitude;
             var lonISS = positionISS.iss_position.longitude;
+
             //On ajoute notre marker
-            marker = new L.marker([latISS, lonISS]);
+            marker = new L.marker([latISS, lonISS], {icon: issIcon});
             marker.addTo(mymap);
-            //On déplace notre caméra
-            mymap.setView([latISS, lonISS], zoom);
+            var latlng = L.latLng(latISS, lonISS);
+
+            //Si la polyline est nulle, ou si on change de signe en longitude (aux valeurs fortes, > 100), on l'initailise
+
+            if(!polyline || (lastLong && (Math.sign(lastLong) != Math.sign(lonISS)) && Math.abs(lonISS) > 100)){
+
+                polyline = new L.polyline(latlng, {color: 'black'});
+
+            //Sinon, on lui ajoute la dernière position
+            } else{
+                polyline.addLatLng(latlng);
+            }
+            polyline.addTo(mymap);
+
+            //On déplace notre caméra le cas échéant
+            if(suiviBtn.checked){
+                mymap.setView([latISS, lonISS], zoom);
+            }
+
             //On met à jour le texte
             latlontxt.innerHTML = latISS + ", " + lonISS;
+
+            //on stocke la dernière longitude (pour le passage à -180°)
+            lastLong = lonISS;
         }
     });
 
